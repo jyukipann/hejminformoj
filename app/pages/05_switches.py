610affc267c625a179243d16a962728166708b8b
jyukipann <jyukipann@yahoo.co.jp>
2024-10-30 16:17:49 +0900

    ホスト名集約とif変形

--- app/pages/05_switches.py
+++ app/pages/05_switches.py
@@ -3,24 +3,29 @@ import requests
 
 left_column, mid_column, right_column = st.columns(3)
 env_data = None
+red_node_host = 'http://192.168.10.250:1880'
 try:
-    env_data = requests.get("http://192.168.10.250:1880/dht22").json()
+    env_data = requests.get(f"{red_node_host}/dht22").json()
 except requests.exceptions.RequestException as e:
     pass
 with left_column:
     if st.button("Light on", use_container_width=True):
-        ret = requests.get("http://192.168.10.250:1880/light?switch=on")
+        ret = requests.get(f"{red_node_host}/light?switch=on")
     if st.button("Night mode", use_container_width=True):
-        ret = requests.get("http://192.168.10.250:1880/light?switch=night")
+        ret = requests.get(f"{red_node_host}/light?switch=night")
     if st.button("Light off", use_container_width=True):
-        ret = requests.get("http://192.168.10.250:1880/light?switch=off")
+        ret = requests.get(f"{red_node_host}/light?switch=off")
 with mid_column:
     if st.button("PC on", use_container_width=True):
-        ret = requests.get("http://192.168.10.250:1880/pc?power=on")
+        ret = requests.get(f"{red_node_host}/pc?power=on")
     if st.button("PC off", use_container_width=True):
-        ret = requests.get("http://192.168.10.250:1880/pc?power=off")
+        ret = requests.get(f"{red_node_host}/pc?power=off")
 with right_column:
-    if st.button(f"Temp {env_data['temp_c']:.1f}℃" if env_data is not None else "Refetch Temp", use_container_width=True):
+    label_temp = f"Temp {env_data['temp_c']:.1f}℃" if (
+        env_data is not None) else "Refetch Temp"
+    label_humid = f"Humid {env_data['humidity']:.1f}%" if (
+        env_data is not None) else "Refetch Humid"
+    if st.button(label_temp, use_container_width=True):
         st.rerun()
-    if st.button(f"Humid {env_data['humidity']:.1f}%" if env_data is not None else "Refetch Humid", use_container_width=True):
+    if st.button(label_humid, use_container_width=True):
         st.rerun()

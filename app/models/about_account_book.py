629313fb1fa8de0e5dc1a24e508a72dde1353ed5
jyukipann <jyukipann@yahoo.co.jp>
2024-10-31 13:18:54 +0900

    transaction typeの追加

--- app/models/about_account_book.py
+++ app/models/about_account_book.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, Date, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey # type: ignore
+from sqlalchemy.ext.declarative import declarative_base # type: ignore
+from sqlalchemy.orm import relationship # type: ignore
 
 Base = declarative_base()
 
@@ -8,17 +8,17 @@ Base = declarative_base()
 class Category(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
 
 class Payer(Base):
     __tablename__ = "payers"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
 
 class TransactionType(Base):
     __tablename__ = "transaction_types"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False, unique=True)
 
 class FinancialTransaction(Base):
     __tablename__ = "financial_transactions"
@@ -29,14 +29,32 @@ class FinancialTransaction(Base):
     amount = Column(Integer, nullable=False)
     is_split_bill = Column(Boolean, default=False)
     
-    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
-    payer_id = Column(Integer, ForeignKey("payers.id"), nullable=False)
-    transaction_type_id = Column(Integer, ForeignKey("transaction_types.id"), nullable=False)
+    category_id = Column(
+        Integer,
+        ForeignKey("categories.id"),
+        nullable=False)
+    payer_id = Column(
+        Integer,
+        ForeignKey("payers.id"),
+        nullable=False)
+    transaction_type_id = Column(
+        Integer,
+        ForeignKey("transaction_types.id"),
+        nullable=False)
     
-    category = relationship("Category", back_populates="transactions")
-    payer = relationship("Payer", back_populates="transactions")
-    transaction_type_id = relationship("TransactionType", back_populates="transactions")
+    category = relationship(
+        "Category",
+        back_populates="transactions")
+    payer = relationship(
+        "Payer",
+        back_populates="transactions")
+    transaction_type = relationship(
+        "TransactionType",
+        back_populates="transactions")
 
-Category.transactions = relationship("FinancialTransaction", back_populates="category")
-Payer.transactions = relationship("FinancialTransaction", back_populates="payer")
-TransactionType.transactions = relationship("FinancialTransaction", back_populates="transaction_type")
+Category.transactions = relationship(
+    "FinancialTransaction", back_populates="category")
+Payer.transactions = relationship(
+    "FinancialTransaction", back_populates="payer")
+TransactionType.transactions = relationship(
+    "FinancialTransaction", back_populates="transaction_type")
